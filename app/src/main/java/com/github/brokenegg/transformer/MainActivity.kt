ca6a4850af9895e093a55bc2bbe99da3a826ede8
Katsuya Iida <katsuya.iida@gmail.com>
2020-07-02 13:53:26 +0900

    Use non-dynamic shape model.

--- app/src/main/java/com/github/brokenegg/transformer/MainActivity.kt
+++ app/src/main/java/com/github/brokenegg/transformer/MainActivity.kt
@@ -1,14 +1,42 @@
 package com.github.brokenegg.transformer
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import org.tensorflow.lite.Interpreter
 
 class MainActivity : AppCompatActivity() {
+
+    val MAX_LEN = 10
+    private var translator: Translator? = null
+    private var editTextInput: EditText? = null
+    private var editTextOutput: EditText? = null
+    private var spinner: Spinner? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val t = Translator(this)
-        t.run()
+
+        translator = Translator(this)
+        editTextInput = findViewById(R.id.editTextInput)
+        editTextOutput = findViewById(R.id.editTextOutput)
+        spinner = findViewById(R.id.spinner)
+        val button = findViewById<Button>(R.id.buttonTranslate)
+        button.setOnClickListener {
+            val pos = spinner!!.getSelectedItemPosition()
+            val langId: Long = when (pos) {
+                0 -> Translator.LANG_ENGLISH
+                1 -> Translator.LANG_SPANISH
+                2 -> Translator.LANG_JAPANESE
+                3 -> Translator.LANG_CHITCHAT
+                else -> 0
+            }
+
+            val inputText = editTextInput!!.text.toString()
+            val outputText = translator!!.run(inputText, langId, MAX_LEN)
+            editTextOutput!!.setText(outputText)
+        }
     }
 }
--- app/src/main/java/com/github/brokenegg/transformer/Translator.java
+++ app/src/main/java/com/github/brokenegg/transformer/Translator.java
@@ -8,6 +8,7 @@ import org.tensorflow.lite.Interpreter;
 import org.tensorflow.lite.gpu.GpuDelegate;
 import org.tensorflow.lite.nnapi.NnApiDelegate;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
@@ -18,7 +19,15 @@ class Translator {
         System.loadLibrary("tokenize");
     }
 
-    private static String MODEL_PATH = "brokenegg_tf23_fp16.tflite";
+    private static final String LOG_TAG = "translator";
+
+    private static final String MODEL_PATH = "brokenegg_tf22.tflite";
+    private static final boolean useDynamicShape = false;
+    private static final long EOS_ID = 2;
+    public static final long LANG_CHITCHAT = 1;
+    public static final long LANG_ENGLISH = 64000;
+    public static final long LANG_SPANISH = 64001;
+    public static final long LANG_JAPANESE = 64002;
     private Interpreter tflite;
 
     public Translator(Activity activity) {
@@ -31,19 +40,23 @@ class Translator {
             long declaredLength = fileDescriptor.getDeclaredLength();
             MappedByteBuffer buffer = fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
             Interpreter.Options opt = new Interpreter.Options();
-            // GpuDelegate gpuDelegate = new GpuDelegate();
-            // opt.addDelegate(gpuDelegate);
+            if (!useDynamicShape) {
+                NnApiDelegate delegate = new NnApiDelegate();
+                opt.addDelegate(delegate);
+            }
             tflite = new Interpreter(buffer, opt);
+            if (!useDynamicShape) {
+                tflite.allocateTensors();
+            }
 
-            byte[] dat = loadModelAsBytes(assetManager, "hoge");
+            byte[] dat = loadModelAsBytes(assetManager, "brokenegg.en-es-ja.spm64k.model");
             initTokenizer(dat, dat.length);
         } catch (Exception ex) {
-
+            Log.d(LOG_TAG, "Exception %s", ex);
         }
     }
 
     private byte[] loadModelAsBytes(AssetManager assetManager, String fileName) {
-        fileName = "brokenegg.en-es-ja.spm64k.model";
         try {
             InputStream inputStream = assetManager.open(fileName);
             byte[] buf = new byte[10000000];
@@ -62,20 +75,49 @@ class Translator {
     private native long[] tokenEncode(String text);
     private native String tokenDecode(long[] ids);
 
-    public void run() {
-        long[] encodedText = tokenEncode("I'm going to the school today.");
-        String decodedText = tokenDecode(encodedText);
+    private long[] padData(long[] ids, int size) {
+        if (ids.length == size) {
+            return ids;
+        }
+        long[] res = new long[size];
+        System.arraycopy(ids, 0, res, 0, ids.length);
+        if (ids.length < size) {
+            res[ids.length] = EOS_ID;
+        }
+        return res;
+    }
+
+    public String run(String text, long langId, int maxLength) {
+        long[] encodedText = tokenEncode(text);
+        if (!useDynamicShape) {
+            encodedText = padData(encodedText, maxLength);
+        }
         long[][] inputData = new long[][] { encodedText };
-        long[][] targetData = new long[][] { new long[10] };
-        targetData[0][0] = 64001;
-        long[][] outputData = new long[][] { new long[9] };
-        tflite.resizeInput(0, new int[] { inputData[0].length, inputData.length });
-        tflite.resizeInput(1, new int[] { targetData[0].length, targetData.length });
-        tflite.allocateTensors();
-        Object[] inputs = new Object[] { inputData, targetData };
-        HashMap<Integer, Object> outputs = new HashMap();
-        outputs.put(0, outputData);
-        tflite.runForMultipleInputsOutputs(inputs, outputs);
-        Log.d("tflite", String.format("Size: %d", outputData.length));
+        long[][] targetData = new long[][] { new long[maxLength] };
+        targetData[0][0] = langId;
+        long[][] outputData = new long[][] { new long[maxLength - 1] };
+
+        int i = 0;
+        for (i = 0; i < maxLength - 1; i++) {
+            if (i == 0 && useDynamicShape) {
+                tflite.resizeInput(0, new int[]{inputData[0].length, inputData.length});
+                tflite.resizeInput(1, new int[]{targetData[0].length, targetData.length});
+                tflite.allocateTensors();
+            }
+            Object[] inputs = new Object[]{inputData, targetData};
+            HashMap<Integer, Object> outputs = new HashMap();
+            outputs.put(0, outputData);
+            tflite.runForMultipleInputsOutputs(inputs, outputs);
+            long predict = outputData[0][i];
+            Log.d("tflite", String.format("Translate: %d", predict));
+            if (predict == EOS_ID) {
+                break;
+            }
+            targetData[0][i + 1] = predict;
+        }
+        long[] result = new long[i];
+        System.arraycopy(targetData[0], 1, result, 0, i);
+        String decodedText = tokenDecode(result);
+        return decodedText;
     }
 }
